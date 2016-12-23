135c12e3a74a2f8b9f097ea299b69359c6be762c
Olivier Refalo <orefalo@yahoo.com>
2016-12-23 09:38:24 -0500

    -g

--- functions/javahome.fish
+++ functions/javahome.fish
@@ -6,14 +6,14 @@ function javahome -d "attempts to set JAVA_HOME"
 
 	set os (uname)
 
-	if test $os = "Darwin"
-       set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+	if test "$os" = "Darwin"
+       set -g -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 	end
 
-	if test $os = "Linux"
-		set -x JAVA_HOME (dirname (dirname (readlink -f (which javac))))
+	if test "$os" = "Linux"
+		set -g -x JAVA_HOME (dirname (dirname (readlink -f (which javac))))
 	end
 
-    set -x PATH $JAVA_HOME/bin $PATH
+    set -g -x PATH $JAVA_HOME/bin $PATH
 
 end
